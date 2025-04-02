b2890b6f6ef584cbcc042c5804138acce288b14b
nkaxpta <nkaxpta@ymail.ne.jp>
2025-04-03 02:53:47 +0900

    Fix: constants.tsを.env参照にするよう変更

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,4 @@
-export const BLOG_TITLE = "エンジニアになった元数学教師の記録";
+export const BLOG_TITLE = process.env.BLOG_TITLE || "default";
 export const NUMBER_OF_ARTICLES_PER_PAGE: number = 10;
 export const OG_IMAGE_DIR_URL =
-  "https://blog-og-image-bucket.s3.ap-northeast-1.amazonaws.com/og-images";
+  process.env.OG_IMAGE_DIR_URL || "http://localhost:3000";
